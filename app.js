8173127ddbdfaf13f5bf9cf701def8e034d5a4a1
Richard <110678590+Amehoboni@users.noreply.github.com>
2023-02-08 08:48:10 +0100

    finishing touch

--- app.js
+++ app.js
@@ -34,7 +34,8 @@ const userSchema = new mongoose.Schema({
     email: String,
     password: String,
     googleId: String,
-    facebookId: String
+    facebookId: String,
+    secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -121,11 +122,42 @@ app.get("/register", function(req, res){
 });
  
 app.get("/secrets", function(req, res){
-    if (req.isAuthenticated()) {
-        res.render("secrets");
+   User.find({"secret": {$ne: null}}, function (err, foundUsers) {
+    if (err) {
+      console.log(err);
     } else {
-        res.redirect("/login");
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
     }
+   });
+});
+
+app.get("/submit", function(req, res){
+  if (req.isAuthenticated()) {
+    res.render("submit");
+} else {
+    res.redirect("/login");
+}
+});
+
+app.post("/submit", function (req, res) {
+  const submittedSecret = req.body.secret;
+
+  // console.log(req.user.id);
+
+  User.findById(req.user.id, function (err, foundUser) {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(function () {
+          res.redirect("/secrets");
+        });
+      }
+    }
+  });
 });
 
 app.get("/logout", function(req, res){
